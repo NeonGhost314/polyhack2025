46573c3997b62d2a5f08205e959b2181b1c689b7
Arzaa <az.poly2024@gmail.com>
2025-02-09 00:21:08 -0500

    logo + slogan + background

--- bloom_wave-app/src/app/page.tsx
+++ bloom_wave-app/src/app/page.tsx
@@ -6,27 +6,31 @@ export default function HomePage() {
 
       <div className="absolute inset-0 z-0">
         <Image
-          src="/world-map.jpg"
+          src="/background.png"
           alt="World map background"
           fill
           className="object-cover"
         />
       </div>
 
-      <div className="absolute inset-0 bg-black opacity-95 z-10"></div>
+      <div className="absolute inset-0 bg-black opacity-20 z-10"></div>
 
       <div className="relative z-20 grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
         <main className="flex flex-col gap-8 row-start-2 items-center justify-center">
           <Image
-            src="/image.png"
+            src="/logo.png"
             alt="Logo BloomWave" 
             width={200} 
             height={200}
             className="mb-4"
           />
-          <h1 className="text-6xl sm:text-8xl font-extrabold text-left  text-blue-300">
-            BloomWave
-          </h1>
+          <div className="flex">
+            <h1 className="text-6xl sm:text-8xl font-extrabold text-left text-white">Bloom</h1>
+            <h1 className="text-6xl sm:text-8xl font-extrabold text-left text-blue-400">Wave</h1>
+          </div>
+          <p className="text-1xl text-center text-white">
+            <h6>A wave of positive change and technological growth.</h6>
+          </p>
         </main>
       </div>
     </div>
