9eb834b55fcdd0301c8a9f972b952ccac927481c
buntep <illuminatiplaying@gmail.com>
2025-02-09 08:15:27 -0500

    Ajouter cors sur ta mere

--- bloom_wave-server/server.js
+++ bloom_wave-server/server.js
@@ -2,6 +2,7 @@ import Fastify from 'fastify'
 import routes from './routes/routes.js';
 import fastifyMongo from "@fastify/mongodb";
 import dotenv from "dotenv";
+import cors from '@fastify/cors'
 
 dotenv.config();
 
@@ -9,6 +10,11 @@ const fastify = Fastify({
   logger: true
 })
 
+await fastify.register(cors, { 
+
+  origin: true // This allows all origins
+
+});
 
 await fastify.register(fastifyMongo, {
   forceClose: true,
