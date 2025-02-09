c8e2041ec65c6142a59bfe45b0da7fa3b1ba18e2
Arzaa <az.poly2024@gmail.com>
2025-02-08 21:10:56 -0500

    accueil.version1

--- bloom_wave-app/src/app/page.tsx
+++ bloom_wave-app/src/app/page.tsx
@@ -1,10 +1,20 @@
 import Image from "next/image";
 
-export default function homePage() {
+export default function HomePage() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start"></main>
-      home-page
+      <main className="flex flex-col gap-8 row-start-2 items-center">
+        <Image
+          src="globe.svg"
+          alt="Logo BloomWave" 
+          width={400} 
+          height={200}
+          className="mb-4"
+        />
+        <h1 className="text-6xl sm:text-8xl font-extrabold text-center">
+          BloomWave
+        </h1>
+      </main>
     </div>
   );
 }
