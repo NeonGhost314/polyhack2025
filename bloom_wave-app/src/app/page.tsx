7374bd40a55a5907c56ef84fb2e89c57d7257923
Arzaa <az.poly2024@gmail.com>
2025-02-09 01:04:26 -0500

    fix

--- bloom_wave-app/src/app/page.tsx
+++ bloom_wave-app/src/app/page.tsx
@@ -15,22 +15,20 @@ export default function HomePage() {
 
       <div className="absolute inset-0 bg-black opacity-20 z-10"></div>
 
-      <div className="relative z-20 grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <div className="relative z-30 items-center min-h-screen p-20 pb-20 gap-10">
         <main className="flex flex-col gap-8 row-start-2 items-center justify-center">
           <Image
             src="/logo.png"
-            alt="Logo BloomWave" 
+            alt="Logo BloomWave"
             width={200} 
             height={200}
             className="mb-4"
           />
           <div className="flex">
-            <h1 className="text-6xl sm:text-8xl font-extrabold text-left text-white">Bloom</h1>
-            <h1 className="text-6xl sm:text-8xl font-extrabold text-left text-blue-400">Wave</h1>
+            <h1 className="text-6xl sm:text-7xl font-extrabold text-left text-white">Bloom</h1>
+            <h1 className="text-6xl sm:text-7xl font-extrabold text-left text-blue-400">Wave</h1>
           </div>
-          <p className="text-1xl text-center text-white">
-            <h6>A wave of positive change and technological growth.</h6>
-          </p>
+          <p className="text-1xl text-center text-white">A wave of positive change and technological growth.</p>
         </main>
       </div>
     </div>
