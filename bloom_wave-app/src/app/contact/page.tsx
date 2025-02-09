c1a03dbaa31dc80ada5595e9a904c4de8678639b
Arzaa <az.poly2024@gmail.com>
2025-02-09 04:45:22 -0500

    profile + contact

--- bloom_wave-app/src/app/contact/page.tsx
+++ bloom_wave-app/src/app/contact/page.tsx
@@ -32,15 +32,15 @@ export default function ContactPage() {
           <h2 className="text-2xl font-bold mb-4 text-gray-800">Contact Us</h2>
           <div className="mb-4">
             <label htmlFor="name" className="block text-gray-700">Name :</label>
-            <input type="text" id="name" name="name" placeholder="Your name" className="mt-1 w-full p-2 border rounded-md"/>
+            <input type="text" id="name" name="name" placeholder="Your name" className="mt-1 w-full p-2 border rounded-md text-black" required/>
           </div>
           <div className="mb-4">
             <label htmlFor="email" className="block text-gray-700">Email :</label>
-            <input type="email" id="email" name="email" placeholder="Your email" className="mt-1 w-full p-2 border rounded-md"/>
+            <input type="email" id="email" name="email" placeholder="Your email" className="mt-1 w-full p-2 border rounded-md text-black" required/>
           </div>
           <div className="mb-4">
             <label htmlFor="message" className="block text-gray-700">Message :</label>
-            <textarea id="message" name="message" rows={5} placeholder="Message" className="mt-1 w-full p-2 border rounded-md resize-none"></textarea>
+            <textarea id="message" name="message" rows={5} placeholder="Message" className="mt-1 w-full p-2 border rounded-md resize-none text-black" required></textarea>
           </div>
           <button type="submit" className="w-full bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-600">Send</button>
         </form>
