e43fca4914de3f39c50dbd4f1e19b34682d6408f
ItsOnlyCed <01.cedav@gmail.com>
2025-02-09 06:55:08 -0500

    the end of server maybe

--- bloom_wave-server/routes/routes.js
+++ bloom_wave-server/routes/routes.js
@@ -2,6 +2,20 @@
 export default async function routes(fastify, options) {
   const db = fastify.mongo.client.db('Informations');
   const signalCollection = db.collection('signals');
+  
+  fastify.get('/signals/fish', async (_, reply) => {
+    const signalCollectionFish = await signalCollection.find({type:"fish"}).toArray();
+    reply.send(signalCollectionFish);
+  });
+  fastify.get('/signals/courant', async (_, reply) => {
+    const signalCollectionCourant = await signalCollection.find({type:"courant"}).toArray();
+    reply.send(signalCollectionCourant);
+  });
+
+  fastify.get('/signals/pollution', async (_, reply) => {
+    const signalCollectionPollution = await signalCollection.find({type:"pollution"}).toArray();
+    reply.send(signalCollectionPollution);
+  });
 
   fastify.get('/signals', async (_, reply) => {
     const signalCollection = await signalCollection.find().toArray();
@@ -9,7 +23,6 @@ export default async function routes(fastify, options) {
   });
 
   fastify.post('/signals', async (request, reply) => {
-    console.log('okokokokok');
     const result = await signalCollection.insertOne(request.body);
     reply.code(201);
     return { id: result.insertedId };
